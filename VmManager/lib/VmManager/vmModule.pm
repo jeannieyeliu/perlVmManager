49a771a4caf8b8616e32fed22f7b5c00dc79b4e0
jeannie.liu <jeannie.liu@sap.com>
2020-09-22 03:57:43 -0700

    update vm module

--- VmManager/lib/VmManager/vmModule.pm
+++ VmManager/lib/VmManager/vmModule.pm
@@ -25,7 +25,7 @@ sub new {
   my $class = shift;
   my $self = {
       _name => shift,
-      _os => shift,
+      _os => shift || 'NULL',
       _storage => shift || 'NULL'
   };
   bless ($self, $class);
@@ -37,20 +37,77 @@ sub create {
   my $self = shift;
   my $createTime = "'2020-09-22 20:10:25'";	
   my $lastUpdate = $createTime;
-  my $checksum = "abcd";
+  my $checksum = "'abcd'";
   my $name = "'".$self->{_name}."'";
   my $os = "'".$self->{_os}."'";
-  my $stmt = qq(INSERT INTO $vmTable (NAME,OS,STORAGE,CREATE_TIME,LAST_UPDATE) VALUES ($name,$os, $self->{_storage},$createTime,$lastUpdate); 
+  my $stmt = qq(INSERT INTO $vmTable (NAME,OS,STORAGE,CHECKSUM,CREATE_TIME,LAST_UPDATE) VALUES ($name,$os, $self->{_storage},$checksum,$createTime,$lastUpdate); 
         );
   # print "Debug: stmt is: $stmt";
   my $rv = $dbh->do($stmt) or error($DBI::errstr); 
-  print "Successfully created vm $self->{_name}\n";
+  if ($rv){ 
+     print "Successfully created vm $self->{_name}\n";
+  }
+  else {
+     print "Faile to create vm $self->{_name}\n";
+  }
 }
+
+sub read {
+  my $self = shift;
+  my $name = "'".$self->{_name}."'";
+  my $stmt = qq(SELECT * from $vmTable where name = $name; );
+  my $sth = $dbh->prepare($stmt);
+  $sth->execute();
+
+  while ( my @row = $sth->fetchrow_array() ) {
+    print join(' ', @row)."\n";
+  }
+  $sth->finish();
+}
+
+# update(name,os,storage);
+sub update {
+  my $self = shift;
+  my $name = shift || $self->{_name};
+  my $name = "'".$name."'";
+  my $os = shift || $self->{_os};
+  my $os = "'".$os."'";
+  my $storage = shift || $self->{_storage};
+  my $lastUpdate = "'2020-10-22 20:10:25'";
+  my $pre_name = "'".$self->{_name}."'";
+  my $updateStr = "NAME=$name, OS=$os,STORAGE=$storage,LAST_UPDATE=$lastUpdate";
+  my $stmt = qq(UPDATE $vmTable set $updateStr where name=$pre_name;);
+  print "update string: $stmt \n";
+  my $rv = $dbh->do($stmt) or error($DBI::errstr);
+  
+  if ($rv){
+    print "Successfully updated vm $self->{_name}\n";
+    # TODO reset obj properties 
+ }
+  else {
+    print "Faile to update vm $self->{_name}\n";
+  }
+  
+}
+
+sub delete {
+ my $self = shift;
+ my $name = "'".$self->{_name}."'";
+ my $stmt = qq(DELETE FROM $vmTable where name=$name;);
+ my $rv = $dbh->do($stmt) or error($DBI::errstr); 
+ if ($rv){
+   print "Successfully deleted vm $self->{_name}\n";
+ }
+ else {
+   print "Faile to delete vm $self->{_name}\n";
+ }
+}
+
 
 sub error {
   my $errMsg = shift;
-  print $errMsg;
-  exit 0;
+  print $errMsg."\n";
+  # exit 0;
 }
 
 sub closeDB{
--- test.pl
+++ test.pl
@@ -4,7 +4,13 @@
 use VmManager::storageModule;
 use VmManager::vmModule;
 # test create
-$vmObj = VmManager::vmModule->new('vmTest2','ubuntu');
+$vmObj = VmManager::vmModule->new('vmTest5','ubuntu');
+$vmObj2 = VmManager::vmModule->new('vmTest1','ubuntu');
+$vmObj3 = VmManager::vmModule->new('vmTest2','centos');
 
+$vmObj->delete();
+$vmObj2->read();
 $vmObj->create();
+$vmObj3->update('vmTest2-updated');
+
 $vmObj->closeDB();
